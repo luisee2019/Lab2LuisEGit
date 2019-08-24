83c56ccdfee3a46aba26a30c5a99a6a23d53423b
CORP\espinal1 <none@espin.com>
2019-08-24 14:02:11 -0400

    New commit with name and description of program assignment

---
+++ src/edu/cscc/Main.java
@@ -0,0 +1,14 @@
+package edu.cscc;
+
+/* Luis Espinal
+082419
+Lab 2 Hello World Program
+This is the Hello World program for Lab 2 GIT
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+	System.out.print("Hello World");
+    }
+}
